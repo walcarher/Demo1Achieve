23435d39a6b9ca81b9f774c427d7cfc84f0051cb
Walther <walcarher@gmail.com>
2019-03-27 17:07:41 +0100

    calls to device monitor

--- DeviceMonitor.py
+++ DeviceMonitor.py
@@ -1,3 +1,33 @@
+#MIT License
+
+# This software has been heavily inspired from: JetsonHacks YouTube videos, examples and GitHub Code
+# Please refer to : https://github.com/jetsonhacks/gpuGraphTX
+
+# Modifications to include instant bar graphs and also CPU average usage history
+# by: Walther Carballo Hernandez
+# Please refer to: https://github.com/walcarher
+# Modifications Copyright (c) 2019 Institut Pascal
+
+#Copyright (c) 2018 Jetsonhacks
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
@@ -6,7 +36,7 @@ devices = ('CPU0', 'CPU1', 'CPU2', 'CPU3', 'CPU4', 'CPU5', 'GPU')
 usage = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 total_last = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 idle_last = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-time_interval = 100 # 50 miliseconds update
+time_interval = 50 # Miliseconds update
 x_axis = np.arange(len(devices))
 # This files are to be found in the L4T version of TX2, this may vary in the future
 gpuUsageFile = '/sys/devices/gpu.0/load'
@@ -18,8 +48,9 @@ plt.ylabel('Usage (%)')
 
 def initGraph():
 	ax.set_ylim(0,100)
-	ln = plt.bar(x_axis[0:7], usage[0:7], width=1.0, color='b', align='center', alpha=0.8)
+	ln = plt.bar(x_axis[0:7], usage[0:7], width=1.0, color='b', align='center', alpha=0.5)
 	ln[6].set_color('g')	
+	ln[6].set_alpha(0.5)	
 	plt.xticks(x_axis, devices)
 	return ln
 
--- pytorch-yolo2/Het_YOLOv2_cam.py
+++ pytorch-yolo2/Het_YOLOv2_cam.py
@@ -1,3 +1,35 @@
+#MIT License
+
+# This software has been heavily inspired from: Marvis GitHub code
+# Please refer to : https://github.com/marvis/
+# Original implementation of YOLOv2 can be found in: https://pjreddie.com/darknet/yolo
+
+# Modifications to include TX2 onboard camera detection, model partitioning and device monitor 
+# by: Walther Carballo Hernandez
+# Please refer to: https://github.com/walcarher
+# Modifications Copyright (c) 2019 Institut Pascal
+
+#Copyright (c) 2015 Preferred Infrastructure, Inc.
+#Copyright (c) 2015 Preferred Networks, Inc.
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+
 import subprocess
 import argparse
 from utils import *
@@ -43,7 +75,7 @@ def demo(cfgfile, weightfile):
     	# Window creation and specifications
         windowName = cfgfile
         cv2.namedWindow(windowName, cv2.WINDOW_NORMAL)
-	cv2.moveWindow(windowName,0,0)
+	cv2.moveWindow(windowName,1920-1280,0)
         cv2.resizeWindow(windowName,1280,1080)
         cv2.setWindowTitle(windowName,"YOLOv2 Object Detection")
         font = cv2.FONT_HERSHEY_PLAIN
@@ -61,7 +93,7 @@ def demo(cfgfile, weightfile):
         if res:
             sized = cv2.resize(img, (m.width, m.height))
             bboxes = do_detect(m, sized, 0.5, 0.4, use_cuda)
-            print('------')
+            #print('------')
             draw_img = plot_boxes_cv2(img, bboxes, None, class_names)
 	    if showHelp == True:
                 cv2.putText(img, helpText, (11,20), font, 1.0, (32,32,32), 4, cv2.LINE_AA)
@@ -90,8 +122,11 @@ def demo(cfgfile, weightfile):
 if __name__ == '__main__':
 	if args.monitor:
 		device_monitor_process = subprocess.Popen(["python", "../DeviceMonitor.py"])
+		gpu_history_process = subprocess.Popen(["python", "../GPUDeviceHistory.py"])
+		cpu_history_process = subprocess.Popen(["python", "../CPUDeviceHistory.py"])
         demo(args.cfg, args.weights)
 	if args.monitor:	
 		device_monitor_process.terminate()
-        #demo('cfg/tiny-yolo-voc.cfg', 'tiny-yolo-voc.weights')
+		gpu_history_process.terminate()
+		cpu_history_process.terminate()
 
